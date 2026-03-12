ccfc12ec3c49dedae24d53b3cc9e12a4b167cc5a
shahdalaa3172003-wq <shahdalaa3172003@gmail.com>
2026-03-12 13:05:13 +0200

    Q1

--- ass02/Program.cs
+++ ass02/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
 {
   public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Q1
+        // Q: What is the purpose of #region and #endregion directives in C#? 
+        //    How do they help in code organization? 
+        //an
+        //How they help in code organization:
+
+//        Group related code such as variables, methods, or properties.
+
+//Improve readability by reducing visible code when sections are collapsed.
+
+//Make navigation easier in large files.
+
+//Keep code structured and organized during development.
+        #endregion
     }
 }
