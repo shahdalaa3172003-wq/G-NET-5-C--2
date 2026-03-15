ec0ac84c6d53f31f5084598e3a2e76b0a532ec3a
shahd Alaa <shahdalaa3172003@gmail.com>
2026-03-15 11:22:48 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,45 +11,63 @@ internal class Program
         //an
         //How they help in code organization:
 
-//        Group related code such as variables, methods, or properties.
+        //        Group related code such as variables, methods, or properties.
 
-//Improve readability by reducing visible code when sections are collapsed.
+        //Improve readability by reducing visible code when sections are collapsed.
 
-//Make navigation easier in large files.
+        //Make navigation easier in large files.
 
-//Keep code structured and organized during development.
+        //Keep code structured and organized during development.
         #endregion
+        #region Q2
+
+        //What is the difference between explicit and implicit variable
+        //    declaration in C#? Provide examples of both.
+        //an
+        //1. Explicit Declaration
+
+        //You manually specify the exact data type.
+        //Syntax: int age = 25;
+
+        //2. Implicit Declaration
+
+        //You use the var keyword, and the compiler infers the type based on the value.
+
+        //Syntax: var age = 25;
+
+        // Explicit
+        //string message = "Hello";
+
+        // Implicit
+        //var message = "Hello";
+
+        #endregion
+        #region Q3
+        //  Write the syntax for declaring a constant in C#. Why would you use 
+        //    a constant instead of a regular variable?
+        // Standard syntax: const [type] [name] = [value];
+        //const double Pi = 3.14159;
+        //const string Greeting = "Hello, World!";
+        //const int MaxPlayers = 10;
+        //Use const for values that never change to prevent bugs, improve readability, and boost performance.
+        #endregion
+        #region Q4
+        // Explain the difference between class-level scope and method-level
+        //    scope with examples.
+        Class - level variables are accessible to the entire class, 
+    while method-level variables exist only inside the specific method where they are declared.
+        public class Calculator
+    {
+        public void CalculateTotal()
+        {
+            int taxAmount = 5;
+            Console.WriteLine(taxAmount);
+        }
+
+        public void ShowTax()
+        {
+        }
     }
+    #endregion
 }
-#region Q2
-
-//What is the difference between explicit and implicit variable
-//    declaration in C#? Provide examples of both.
-//an
-//1. Explicit Declaration
-
-//You manually specify the exact data type.
-//Syntax: int age = 25;
-
-//2. Implicit Declaration
-
-//You use the var keyword, and the compiler infers the type based on the value.
-
-//Syntax: var age = 25;
-
-// Explicit
-//string message = "Hello";
-
-// Implicit
-//var message = "Hello";
-
-#endregion
-region Q3
-  //  Write the syntax for declaring a constant in C#. Why would you use 
-  //    a constant instead of a regular variable?
-  // Standard syntax: const [type] [name] = [value];
-  //const double Pi = 3.14159;
-  //const string Greeting = "Hello, World!";
-  //const int MaxPlayers = 10;
-  //Use const for values that never change to prevent bugs, improve readability, and boost performance.
-#endregion
+}
