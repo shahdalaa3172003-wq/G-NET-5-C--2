b1b6eae4a92ec75f0cdc8b7d71e985432c0a997c
shahd Alaa <shahdalaa3172003@gmail.com>
2026-03-15 11:08:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+  public static void Main(string[] args)
+    {
+        #region Q1
+        // Q: What is the purpose of #region and #endregion directives in C#? 
+        //    How do they help in code organization? 
+        //an
+        //How they help in code organization:
+
+//        Group related code such as variables, methods, or properties.
+
+//Improve readability by reducing visible code when sections are collapsed.
+
+//Make navigation easier in large files.
+
+//Keep code structured and organized during development.
+        #endregion
+    }
+}
+#region Q2
+
+//What is the difference between explicit and implicit variable
+//    declaration in C#? Provide examples of both.
+//an
+//1. Explicit Declaration
+
+//You manually specify the exact data type.
+//Syntax: int age = 25;
+
+//2. Implicit Declaration
+
+//You use the var keyword, and the compiler infers the type based on the value.
+
+//Syntax: var age = 25;
+
+// Explicit
+//string message = "Hello";
+
+// Implicit
+//var message = "Hello";
+
+#endregion
